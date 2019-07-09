589375b49981f6e2df266bd21c06f77b6c947a53
Makoto Ishii <b52keeptype@gmail.com>
2019-07-09 19:19:24 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/works'
+  get  'static_pages/cv'
+  get  'static_pages/contact'
 
-  get 'static_pages/works'
-
-  get 'static_pages/cv'
-
-  get 'static_pages/contact'
-
-  root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,38 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+   @base_title = "Makoto Ishii"
+ end
+
+ test "should get root" do
+     get root_url
+     assert_response :success
+   end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get works" do
     get static_pages_works_url
     assert_response :success
+    assert_select "title", "Works | #{@base_title}"
   end
 
   test "should get cv" do
     get static_pages_cv_url
     assert_response :success
+    assert_select "title", "Cv | #{@base_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
